bfc659c5a19f19ed1dac739c2ec19a85aca1b6a8
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-02-23 17:13:43 +0330

    setting up project

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8cly2PCzReXcMxfgGzQjixGmWFb8VbQ8",
+  authDomain: "dinsey-clone-10c88.firebaseapp.com",
+  projectId: "dinsey-clone-10c88",
+  storageBucket: "dinsey-clone-10c88.appspot.com",
+  messagingSenderId: "83965870364",
+  appId: "1:83965870364:web:e6720b0b8de78812d53ad9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
